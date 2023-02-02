ae0fafdc69c73886b936d6b88f23c8cd7b862ea3
dombosfi <filip.dombos@gmail.com>
2023-02-02 13:35:21 +0100

    Skeleton

---
+++ front_oederlin_erp/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+  export const supabase = createClient('https://gxtplfrkgbeikpqprdof.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4dHBsZnJrZ2JlaWtwcXByZG9mIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzUzMzcyMjUsImV4cCI6MTk5MDkxMzIyNX0.04qq_2lo4yy3ri4s3GWSYg37vjI2jvmj3Gt0YHGdlgk')  
---
+++ front_oederlin_erp/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let data;
+    let { users } = data;
+    $: ({ users } = data);
+</script>
+
+<ul>
+    {#each users as user}
+        <li>{user.firstname + " " + user.lastname + " joined " + user.joined}</li>
+    {/each}
+</ul>
