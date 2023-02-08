f3c2c84190316520eaadb076556d1b659c92cb74
Filip Dombos <filip.dombos@gmail.com>
2023-02-08 13:00:29 +0100

    StoreItems update

---
+++ front_oederlin_erp/src/lib/StoreUI/StoreItem.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { Group } from 'flowbite-svelte';
+
+	export let itemName = 'Suit';
+	export let item = {};
+
+	function click() {
+		console.log('You clicked!');
+	}
+</script>
+
+<div
+	class="rounded-md hover:bg-gray-600"
+	on:click={click}
+	on:keypress
+>
+	<div class="text-center, text-4xl">
+		{item.name}
+	</div>
+	<div class="text-center">
+		Number of items left: {item.quantity}
+	</div>
+</div>
---
+++ front_oederlin_erp/src/lib/StoreUI/StorePanel.svelte
@@ -0,0 +1,37 @@
+<script>
+	import StoreItem from '$lib/StoreUI/StoreItem.svelte';
+	export let item = {
+		name: 'Suit',
+		quantity: 3
+	};
+
+	// user preference module
+	let col = 3;
+	let todo = {
+		done: false
+	};
+
+	$: console.log(col);
+</script>
+
+<!-- The userinterface of the Store-->
+<div>
+	<div class="container flex justify-end bg-blue-500">
+		<label>{col} Columns</label>
+		<input
+			class="ml-4"
+			type="range"
+			bind:value={col}
+			min="1"
+			max="20"
+		/>
+	</div>
+</div>
+
+<div class="grid gap-4 md:grid-cols-3">
+	{#each Array(20) as _}
+		<StoreItem {item} />
+	{/each}
+</div>
+
+<div class:done={todo.done} />
