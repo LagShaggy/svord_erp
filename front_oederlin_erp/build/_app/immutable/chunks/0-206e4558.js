1838d4a0dad666bc5ce5042bf5c45964e74fee4d
Filip Dombos <filip.dombos@gmail.com>
2023-02-28 18:18:25 +0100

    build:move border from Description.svelte to page.svelte

--- front_oederlin_erp/build/_app/immutable/chunks/0-206e4558.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-1fd7b42d.js";export{t as component};
---
+++ front_oederlin_erp/build/_app/immutable/chunks/7-e6ccc9a3.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-aaa6236c.js";import{default as t}from"../components/pages/products/item/_slug_/_page.svelte-986173b2.js";export{t as component,r as universal};
