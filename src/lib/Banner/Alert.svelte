a26c6f9394c6ab8735029b0824be71e885784ce4
dombosfi <filip.dombos@gmail.com>
2023-04-03 11:55:53 +0200

    still need to fix destroy of alert on:click

--- src/lib/Banner/Alert.svelte
+++ src/lib/Banner/Alert.svelte
@@ -2,8 +2,7 @@
 	import { alert } from '$src/stores.js'
 	import { onDestroy, onMount } from 'svelte'
 
-	let alertInfo = {}
-	let colour = 'red'
+	let alertInfo = $alert
 
 	const unsubscribe = alert.subscribe(
 		(/** @type {{}} */ value) => (alertInfo = value)
@@ -12,11 +11,11 @@
 	onMount(() => {})
 
 	onDestroy(() => {
-		unsubscribe
+		unsubscribe()
 	})
 
 	function destroy() {
-		alertInfo = ''
+		alertInfo.set(null)
 		console.log('Alter says Goodbye')
 	}
 	console.log('Hi from the Alert : ' + alert)
@@ -25,7 +24,8 @@
 {#if alertInfo}
 	<div
 		class="absolute top-6 left-1/2 z-20 -translate-x-1/2 -translate-y-1/2
-		transform rounded-md border bg-{alertInfo.colour}-300 mt-3 p-2 px-5 py-1 text-center sm:mx-2"
+		transform rounded-md border bg-{alertInfo.colour}-300 mt-3 p-2 px-5 py-1 
+		text-center sm:mx-2"
 		on:click={destroy}
 		on:keydown={destroy}
 	>
