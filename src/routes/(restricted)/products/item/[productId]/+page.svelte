7f584f9223adaf00bbd6b47605acbc0a1e9170a5
LagShaggy <filip.dombos@gmail.com>
2024-03-28 00:15:46 +0100

    add an accordion component

--- src/routes/(restricted)/products/item/[productId]/+page.svelte
+++ src/routes/(restricted)/products/item/[productId]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Accordion from '$src/lib/UI/Accordion/Accordion.svelte'
 	import BasePage from '$src/lib/UI/BasePage.svelte'
 	import type { PageData } from './$types'
 
@@ -7,4 +8,15 @@
 	$: ({ product } = data)
 </script>
 
-<BasePage title={product.name}></BasePage>
+<BasePage title={product.name}>
+	<Accordion
+		props={{
+			title: 'Product Informartion',
+			shortDescr: 'Here you can see an overview of the basic product properties'
+		}}
+	></Accordion>
+	<Accordion props={{ title: 'Workflows', shortDescr: 'See product workflows' }}></Accordion>
+	<Accordion props={{ title: 'People of Interest', shortDescr: 'See product workflows' }}
+	></Accordion>
+	<Accordion props={{ title: 'Comments', shortDescr: 'See product workflows' }}></Accordion>
+</BasePage>
