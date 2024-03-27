62295aecef1a224cf2a01767dcbb6a7a6ed1ab37
LagShaggy <filip.dombos@gmail.com>
2024-03-27 23:34:21 +0100

    implement redirect when clicking items

--- src/lib/UI/Table/Rows/ProductRow.svelte
+++ src/lib/UI/Table/Rows/ProductRow.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
+	import { redirect } from '@sveltejs/kit'
 	import TableData from '../Molecules/TableData.svelte'
 	import { getColourForIndex } from '../Molecules/tableHelpers'
 	import TableRow from '../Molecules/TableRow.svelte'
+	import { goto } from '$app/navigation'
 
 	export let item
 	export let index: number
 	const colour = getColourForIndex(index)
 	const handleClick = (e: MouseEvent) => {
-		console.log('hello')
+		goto(`/products/item/${item.id}`)
 	}
 </script>
 
