5cb40022cc36993a2ff2a1b4870ac84c94cc8550
Filip Dombos <filip.dombos@gmail.com>
2024-04-11 10:52:09 +0200

    changing around layout for restricted and overall for better data loading logics

---
+++ src/routes/(restricted)/+layout.server.ts
@@ -0,0 +1,46 @@
+import type { Session } from '@supabase/supabase-js'
+import type { LayoutServerLoad } from './$types'
+import type { Profile } from '$src/lib/supabase/schema'
+
+export const load: LayoutServerLoad = async ({ locals: { getSession, supabase } }) => {
+	const getProfile = async (session: Session | undefined) => {
+		if (!session) {
+			return undefined
+		}
+		const { data: profileData, error: profileError } = await supabase
+			.from('Profile')
+			.select()
+			.eq('id', session!.user.id)
+			.single()
+		if (profileError) {
+			throw profileError
+		}
+		return profileData
+	}
+
+	const getProfilePicture = async (profile: Profile | undefined) => {
+		if (!profile) {
+			return undefined
+		}
+		if (profile?.avatar_url) {
+			const { data: picUrl, error: picError } = await supabase.storage
+				.from('images')
+				.createSignedUrl(profile.avatar_url, 6000)
+
+			if (picError) {
+				throw picError
+			}
+
+			return picUrl?.signedUrl ?? undefined
+		}
+	}
+
+	try {
+		const profile = await getProfile((await getSession()) ?? undefined)
+		const profilePicture = await getProfilePicture(profile)
+		console.log(profilePicture)
+		return { profile, profilePicture }
+	} catch (e) {
+		console.log(e)
+	}
+}
