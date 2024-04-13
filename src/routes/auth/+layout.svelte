fbfdf74ef9a35e2ee3e02b2267ef013900a95eeb
Filip Dombos <filip.dombos@gmail.com>
2024-04-13 09:33:16 +0200

    upate Login form, remove component

--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -2,10 +2,8 @@
 	import '$src/app.css'
 	import SvordxCompanyLogo from '$lib/Icons/SvordxCompanyLogo.svelte'
 	import HeadBar from '$lib/UI/TopBar/HeadBar.svelte'
-	import AlertBar from '$src/lib/Alert/AlertBar.svelte'
 </script>
 
-<AlertBar />
 <HeadBar>
 	<SvordxCompanyLogo />
 </HeadBar>
--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
-	import type { ActionData } from './$types'
-	import LoginForm from '$lib/UI/Forms/LoginForm.svelte'
-	import { ACTIONS } from '$lib/routes'
-
-	export let form: ActionData
+	import Form from '$src/lib/UI/Primitive/Form/Form.svelte'
+	import { ACTIONS } from '$src/lib/routes'
+	import InputEmail from '$src/lib/UI/Primitive/Form/InputEmail.svelte'
+	import { page } from '$app/stores'
+	import InputPassword from '$src/lib/UI/Primitive/Form/InputPassword.svelte'
+	import SubmitButton from '$src/lib/UI/Primitive/Form/SubmitButton.svelte'
 </script>
 
-<LoginForm />
+<Form action={ACTIONS.AUTH.LOGIN} className={'w-1/3'}>
+	<h1>Welcome back</h1>
+	<InputEmail name={'email'} value={$page.form?.email}>Email</InputEmail>
+	<InputPassword name={'password'}>Password</InputPassword>
+	<SubmitButton className={'mt-5'}>Login</SubmitButton>
+</Form>
