fc3242d49c2c7a6ba12e531ddf42fab6797f96fa
Filip Dombos <filip.dombos@gmail.com>
2024-05-09 23:10:27 +0200

    create framework for country flags

---
+++ src/lib/Icons/Flags/CountryFlag.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { Country } from './countries.enum'
+
+	export let country: Country
+	let code = country.toLocaleLowerCase()
+
+	// fi is a library for country flags that is imported in the app.html over cdn
+</script>
+
+<span class="fi fi-{code}"></span>
