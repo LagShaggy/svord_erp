98f042d00facf2284cb9de03f2e54f78acb0e7fd
Filip Dombos <filip.dombos@gmail.com>
2024-09-10 10:34:01 +0200

    add download trash and upload icon

---
+++ src/lib/Icons/UploadIcon.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let className: String = ''
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="24"
+	height="24"
+	viewBox="0 0 24 24"
+	stroke-width="2"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	class="stroke-black fill-transparent {className}"
+>
+	<polyline points="16 16 12 12 8 16"></polyline><line x1="12" y1="12" x2="12" y2="21"></line><path
+		d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"
+	>
+	</path><polyline points="16 16 12 12 8 16"></polyline>
+</svg>
